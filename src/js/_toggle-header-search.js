97001e1599a940be676b1c3d812cff78645a640b
Glenn McComb <lenymo@gmail.com>
2018-05-07 20:14:27 +1000

    Site header search.

--- src/js/_toggle-header-search.js
+++ src/js/_toggle-header-search.js
@@ -8,13 +8,62 @@ var ToggleHeaderSearch = (function() {
 
 
   //
+  //  CONFIG
+  //––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  var config = {
+    searchToggleClass: '-search-is-open'
+  };
+  
+
+
+  //
   //  TOGGLE SEARCH
   //––––––––––––––––––––––––––––––––––––––––––––––––––
 
-  function toggleSearch() {
+  function toggleHeaderSearch() {
 
     // Code goes here.
+    var headerSearchInput = document.querySelector('.header-search__input');
+
+    // If the search input exists.
+    if ( headerSearchInput ) {
+
+      // Listen for the focus event.
+      headerSearchInput.addEventListener('focus', showSearch, false);
+
+      // Listen for the blue event.
+      headerSearchInput.addEventListener('blur', hideSearch, false);
+    }
   }
+
+
+  //
+  //  SHOW SEARCH
+  //––––––––––––––––––––––––––––––––––––––––––––––––––
+
+  function showSearch( e ) {
+    
+    // Get the body elem.    
+    var bodyElem = document.querySelector('body');
+
+    bodyElem.classList.add( config.searchToggleClass );
+  }
+
+
+  //
+  //  HIDE SEARCH
+  //––––––––––––––––––––––––––––––––––––––––––––––––––
+  
+  function hideSearch( e ) {
+
+    // Get the body elem.
+    var bodyElem = document.querySelector('body');
+
+    bodyElem.classList.remove( config.searchToggleClass );
+  }
+
+  
 
 
   //
@@ -22,7 +71,7 @@ var ToggleHeaderSearch = (function() {
   //––––––––––––––––––––––––––––––––––––––––––––––––––
 
   function init() {
-    toggleSearch();
+    toggleHeaderSearch();
   }
 
 
